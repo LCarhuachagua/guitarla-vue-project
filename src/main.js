7ee78589b6ad23307bc85ed38ab734e437072e7e
LCarhuachagua <roselyn199206@gmail.com>
2023-10-17 12:50:33 -0500

    from html, css to vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue' // importa la funcion createApp de vue para crear la app 
+import './style.css' // importa el css para que se aplique a toda la app
+import App from './App.vue' // importa el componente App.vue que es el componente principal de la app
 
-createApp(App).mount('#app')
+createApp(App).mount('#app') // mount the app, se usa el # para indicar el id del elemento html
