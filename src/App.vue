b0ba615b564f81d24ad1c259ed07033b08702ad2
LCarhuachagua <roselyn199206@gmail.com>
2023-10-17 12:16:40 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<!-- <script setup>
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style> -->
+
+<script setup>
+  const hola = "hola mundo"
+</script>
+
+<template>
+  <h1>{{ hola }}</h1>
+</template>
+
+<style>
+  h1 {
+    text-transform: uppercase;
+    color: red;
+  }
+</style>
+
