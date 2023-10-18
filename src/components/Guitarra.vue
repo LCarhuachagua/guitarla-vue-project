c1016c870bc84a49b5fd95da2547122d77ac1993
LCarhuachagua <roselyn199206@gmail.com>
2023-10-18 11:51:53 -0500

    add inline y method  handlers

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -1,10 +1,19 @@
 <script setup>
+    import { ref } from 'vue'
+
+    const numero = ref(0) 
+
     const props = defineProps({
         propGuitar: {
             type: Object,
             required: true
         }
     })
+
+    const incrementar = () => {
+        numero.value++
+    }
+
 </script>
 
 <template>
@@ -21,10 +30,20 @@
         {{ propGuitar.nombre }}
       </h3>
       <p>
+        {{ numero }}
+      </p>
+      <p>
         {{ propGuitar.descripcion }}
       </p>
       <p class="fw-black text-primary fs-3">$ {{ propGuitar.precio }}</p>
-      <button type="button" class="btn btn-dark w-100">
+      <button
+        type="button" 
+        class="btn btn-dark w-100"
+        @click="incrementar" 
+      >
+      <!-- v-on:click="numero++" es un evento inline-handler -->
+      <!-- v-on:click="incrementar" es un evento method-handler, se tiene que llamar una función arriba dentro de script -->
+      <!-- los eventos no funcionan con solo : si no con un @ por ejemplo en vez de v-on:click sería @click -->
         Agregar al Carrito
       </button>
     </div>
